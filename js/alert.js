6d22156b2e17756be2169b82ac91dbac07591c0e
Arvin202 <h7t6jj22@gmail.com>
2021-08-23 14:59:06 +0800

    market page finished

---
+++ js/alert.js
@@ -0,0 +1,10 @@
+const oil = document.getElementById('oil');
+const oilUsing = document.getElementById('oilUsing');
+
+oil.addEventListener('click', () => {
+  alert(" ✨牽起紅線-正緣現身魔法油 \r ✨翻雲覆雨-火熱性愛魔法油 \r ✨轉角遇到愛-遇見真愛魔法油 \r ✨邱比特之箭-桃花旺旺魔法油");
+});
+
+oilUsing.addEventListener('click', () => {
+  alert("👉🏻打開瓶蓋 \r 👉🏻手比劍指 \r 👉🏻順時針繞魔藥瓶口 \r 👉🏻觀想畫面 \r 👉🏻念誦三次咒語(一次比一次更快，唸出聲音) \r 👉🏻深吸一口氣吹入瓶中 \r 👉🏻蓋上瓶蓋 \r 👉🏻搖晃均勻(混和自身魔力與魔藥) \r 👉🏻使用魔法油");
+});
